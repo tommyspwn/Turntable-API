e589234100fdd4ccc792edcba541d8bb6554b2b0
tommyspwn <tommyspwn@aol.com>
2012-10-28 18:21:35 -0300

    Update examples/autobop.js

--- examples/autobop.js
+++ examples/autobop.js
@@ -4,10 +4,10 @@
  * For more information, visit http://faq.turntable.fm/customer/portal/articles/258935
  */
 
-var Bot    = require('../index');
-var AUTH   = 'auth+live+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
-var USERID = 'xxxxxxxxxxxxxxxxxxxxxxxx';
-var ROOMID = 'xxxxxxxxxxxxxxxxxxxxxxxx';
+var Bot    = require('ttapi);
+var AUTH   = 'auth+live+LryxXvVGOAHKfNKtwaRNUlQP';
+var USERID = '50874b1feb35c174010000da';
+var ROOMID = '4e29cefc14169c27bf255fad';
 
 var bot = new Bot(AUTH, USERID, ROOMID);
 
